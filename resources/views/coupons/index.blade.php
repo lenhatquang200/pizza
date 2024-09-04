e2be9760b35201decf998024bd9902d8596c6b06
popivn <popixworkspace@gmail.com>
2024-09-04 22:10:54 +0700

    update

--- resources/views/coupons/index.blade.php
+++ resources/views/coupons/index.blade.php
@@ -7,24 +7,24 @@
             @foreach ($coupons as $coupon)
                 <div class="carousel-slide">
                     @if(!empty($coupon->url) && $coupon->url !== '#:' && $coupon->url !== '#')
-                    <a href="{{ $coupon->url }}" class="carousel-link">
-                        <img src="{{ asset('storage/' . $coupon->bannerurl) }}">
-                    </a>
+                        <a href="{{ $coupon->url }}" class="carousel-link">
+                            <img src="{{ asset('storage/' . $coupon->bannerurl) }}">
+                        </a>
                     @else
-                        <a class="carousel-link">
-                            <img style="pointer-events: none;" src="{{ asset('storage/' . $coupon->bannerurl) }}">
+                        <a class="carousel-link no-link">
+                            <img src="{{ asset('storage/' . $coupon->bannerurl) }}">
                         </a>
                     @endif
 
-                    <a href="#" class=" btn-coupon btn-view-menu text-uppercase" data-code="{{ $coupon->couponcode  }}">
-                        CODE: {{ $coupon->couponcode  }}
+                    <a href="#" class="btn-coupon btn-view-menu text-uppercase" data-code="{{ $coupon->couponcode }}">
+                        CODE: {{ $coupon->couponcode }}
                         <i class="fa-regular fa-copy"></i>
                     </a>
                 </div>
             @endforeach
         </div>
     @else
-    @include('partials.no-content')
+        @include('partials.no-content')
     @endif
 </div>
 @endsection
--- resources/views/partials/coupons.blade.php
+++ resources/views/partials/coupons.blade.php
@@ -3,9 +3,16 @@
         @foreach ($coupons as $coupon)
             <div class="carousel-slide">
                 <div class="text-overlay-wrapper">
-                    <a href="{{ $coupon->url }}" class="carousel-link">
-                        <img src="{{ asset('storage/' . $coupon->bannerurl) }}" alt="Coupon Image {{ $loop->index + 1 }}">
-                    </a>
+                    @if(!empty($coupon->url) && $coupon->url !== '#:' && $coupon->url !== '#')
+                        <a href="{{ $coupon->url }}" class="carousel-link">
+                            <img src="{{ asset('storage/' . $coupon->bannerurl) }}" alt="Coupon Image {{ $loop->index + 1 }}">
+                        </a>
+                    @else
+                        <a class="carousel-link no-link">
+                            <img src="{{ asset('storage/' . $coupon->bannerurl) }}" alt="Coupon Image {{ $loop->index + 1 }}">
+                        </a>
+                    @endif
+
                     @if($overlayTextprimary || $overlayTextsecondary || $buttonUrl)
                         <div class="text-overlay-container">
                             @if($overlayTextprimary)
@@ -14,10 +21,10 @@
                             @if($overlayTextsecondary)
                                 <div class="text-overlay-secondary">{{ $overlayTextsecondary }}</div>
                             @endif
-                                    <a href="#" class=" btn-coupon btn-view-menu text-uppercase" data-code="{{ $coupon->couponcode  }}">
-                                        CODE: {{ $coupon->couponcode  }}
-                                        <i class="fa-regular fa-copy"></i>
-                                    </a>
+                            <a href="#" class="btn-coupon btn-view-menu text-uppercase" data-code="{{ $coupon->couponcode  }}">
+                                CODE: {{ $coupon->couponcode  }}
+                                <i class="fa-regular fa-copy"></i>
+                            </a>
                         </div>
                     @endif
 
@@ -31,5 +38,5 @@
         @endforeach
     </div>
 @else
-@include('partials.no-content')
+    @include('partials.no-content')
 @endif
