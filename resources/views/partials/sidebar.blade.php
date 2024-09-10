e161200ed3df26998ba082b54b01360ef641c9b5
Q <nhatquang2106qh@gmail.com>
2024-09-10 22:23:07 +0700

    change logo

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -3,10 +3,7 @@
     <div class="close-btn" id="closeBtn">
         <i class="fas fa-times"></i>
     </div>
-    <a class="navbar-brand" href="/">
-        <img style="    filter: invert(1);
-" src="{{ $settings['brand_logo']->image_url ?? asset('storage/images/logo.png') }}">
-    </a>
+
     <ul class="nav_list">
         <li class="nav_item"><a href="/" class="nav_link">Home</a></li>
         <li class="nav_item"><a href="/menu" class="nav_link">Menu</a></li>
