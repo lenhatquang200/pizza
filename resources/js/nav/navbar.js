9939d969a482cb278b636bf4007af69d8c26b422
Q <nhatquang2106qh@gmail.com>
2024-09-10 23:48:09 +0700

    update git

--- resources/js/nav/navbar.js
+++ resources/js/nav/navbar.js
@@ -3,35 +3,23 @@ $(document).ready(function() {
     const sidebar = $('#sidebarMenu');
     const closeBtn = $('#closeBtn');
 
-    // Toggle sidebar when hamburger menu is clicked
     hamburgerMenu.on('click', function() {
         sidebar.toggleClass('show');
     });
 
-    // Close sidebar when close button is clicked
     closeBtn.on('click', function() {
         sidebar.removeClass('show');
     });
 
-    // Click handling for closing sidebar or following links inside
     $(document).on('click', function(e) {
-
-        // If the sidebar is open
         if (sidebar.hasClass('show')) {
-            // Check if click is outside sidebar and hamburger menu
             if (!sidebar.is(e.target) && sidebar.has(e.target).length === 0 &&
                 !hamburgerMenu.is(e.target) && hamburgerMenu.has(e.target).length === 0) {
-
-                // Prevent link follow if clicked outside the sidebar
-                    e.preventDefault();
-
-                // Close the sidebar if the click is outside
-                sidebar.removeClass('show');
+                 e.preventDefault();
+                 sidebar.removeClass('show');
             }
         }
     });
-
-    // Allow sidebar links to work normally
     sidebar.on('click', 'a', function(e) {
         // Let links inside the sidebar work normally
         e.stopPropagation(); // Prevents the click event from propagating to the document
--- resources/views/partials/navbar_menu.blade.php
+++ resources/views/partials/navbar_menu.blade.php
@@ -1,15 +1,17 @@
 <nav class="navbar_menu">
-    <div class="container">
+    <div class="container-fluid">
         <div class="hamburger-menu" id="hamburger-menu">
             <i class="hamburger-icon fas fa-bars"></i>
-            <a class="navbar-brand" style="text-align: center;
-    flex: auto;" href="/">
-                <img src="{{ $settings['brand_logo']->image_url ?? asset('storage/default/logo.png') }}">
-            </a>
+            <div class="navbar-brand" style="text-align: center;
+    flex: auto;" >
+                <a href="{{ route('home') }}">
+                <img     style="height: 60px;" src="{{ $settings['brand_logo']->image_url ?? asset('storage/default/logo.png') }}">
+                </a>
+            </div>
         </div>
 
         <ul class="nav_list ">
-            <a class="navbar-brand" href="{{ route('home') }}">
+            <a class="navbar-brand me-2" href="{{ route('home') }}">
                 <img style="height: 60px" src="{{ $settings['brand_logo']->image_url ?? asset('storage/default/logo.png') }}">
             </a>
             <li class="nav_item">
@@ -35,13 +37,15 @@
             </li>
 
             <li class="nav_item"><a href="https://piazzaorsillo.pdqonlineordering.com/Titlepage.aspx" class="nav_link">Order Online</a></li>
-            <i class="fas text-white fa-flip-horizontal fa-2x fa-phone-alt phone-icon"></i>
+            <div class="d-none nav_item d-xl-flex w-auto">
+                <i class="fas text-white fa-flip-horizontal fa-2x fa-phone-alt phone-icon"></i>
 
-            <div class="contact-numbers text-white">
-                                    <span>732-805-9506</span>
-                <br>
-                                    <span>732-805-9507</span>
-                                </div>
+                <div class="contact-numbers ms-2 text-white">
+                    <span>732-805-9506</span>
+                    <br>
+                    <span>732-805-9507</span>
+                </div>
+            </div>
         </ul>
     </div>
 </nav>
